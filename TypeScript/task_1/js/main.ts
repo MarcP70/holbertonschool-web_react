d740c96db1e22b91f78ebfd52f5368121ec56357
MarcP70 <marcpourias@yahoo.fr>
2024-08-13 17:27:23 +0200

    Add files for task 1

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,45 @@
+// task 1
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+}
+
+// task 2
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+// task 3
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+// task 4
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return `${this.firstName}`;
+  }
+}
