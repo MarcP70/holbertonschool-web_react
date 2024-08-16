d20486d881942bdf19049ac3f66e31f7164baf11
MarcP70 <marcpourias@yahoo.fr>
2024-08-16 12:10:19 +0200

    Fix webpack.config.js in contentBase ./public

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
+    contentBase: path.join(__dirname, './public'),
     port: 8564,
     open: true,
   },
