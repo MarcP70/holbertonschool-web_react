029b5df48a90996f3ed66572dcdc271ff48a5ef7
MarcP70 <marcpourias@yahoo.fr>
2024-08-16 12:06:58 +0200

    Fix webpack.config.js to replace static by contentBase

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -15,9 +15,7 @@ module.exports = {
   },
   devtool: 'inline-source-map',
   devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
-    },
+    contentBase: path.join(__dirname, 'public'),
     port: 8564,
     open: true,
   },
