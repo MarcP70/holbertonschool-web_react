dc9b61eccf1e8abd7b5d5882cf69d12787691a5d
MarcP70 <marcpourias@yahoo.fr>
2024-08-20 16:09:55 +0200

    update setupTests.js

--- react_intro/Task_3/dashboard/src/setupTests.js
+++ react_intro/Task_3/dashboard/src/setupTests.js
@@ -1,10 +1,4 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import { TextEncoder, TextDecoder } from 'util';
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-// Polyfill pour TextEncoder et TextDecoder
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
-
-// Configuration d'Enzyme avec l'adaptateur React 16
-configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() })
