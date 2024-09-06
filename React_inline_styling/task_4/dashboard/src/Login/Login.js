6b41a7026a46cea8265ed57787ed0ed9bcd73bef
MarcP70 <marcpourias@yahoo.fr>
2024-09-06 11:07:06 +0200

    Add files for task 4

---
+++ React_inline_styling/task_4/dashboard/src/Login/Login.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+
+function Login() {
+  return (
+      <div className={css(styles.body)}>
+        <p>Login to access the full dashboard</p>
+        <form className={css(styles.form)}>
+          <div className={css(styles.block)}>
+            <label htmlFor="email" className={css(styles.label)}>Email:</label>
+            <input className={css(styles.input)} type="email" id="email" name="email" />
+          </div>
+          <div className={css(styles.block)}>
+            <label htmlFor="password" className={css(styles.label)}>Password:</label>
+            <input className={css(styles.input)} type="password" id="password" name="password" />
+          </div>
+          <button className={css(styles.button)} type="submit">OK</button>
+        </form>
+      </div>
+  );
+}
+
+const styles = StyleSheet.create({
+  body: {
+    fontSize: '20px',
+    fontFamily: 'Arial, sans-serif',
+  },
+  button: {
+    borderRadius: '4px',
+    border: '1px solid #ccc',
+    backgroundColor: 'white',
+  },
+  block: {
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: '10px',
+    marginRight: '20px',
+  },
+  form: {
+    display: 'inline-block',
+    '@media (min-width: 900px)': {
+      display: 'flex',
+    },
+  },
+  label: {
+    marginRight: '10px',
+    fontWeight: 'bold',
+  },
+  input: {
+    border: '1px solid #ccc',
+    padding: '5px',
+  },
+});
+
+export default Login;
---
+++ React_inline_styling/task_4/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+import PropTypes from 'prop-types';
+import NotificationItem from './NotificationItem';
+import NotificationItemShape from './NotificationItemShape';
+
+class Notifications extends React.Component {
+  static propTypes = {
+    displayDrawer: PropTypes.bool,
+    listNotifications: PropTypes.arrayOf(NotificationItemShape)
+  }
+
+  static defaultProps = {
+    displayDrawer: false,
+    listNotifications: []
+  }
+
+  markAsRead = id => {
+    console.log(`Notification ${id} has been marked as read`);
+  }
+
+  handleClose = () => {
+    console.log('Close button has been clicked');
+  }
+
+  shouldComponentUpdate(nextProps) {
+    // Only update if the new listNotifications has more items than the current list
+    return nextProps.listNotifications.length > this.props.listNotifications.length;
+  }
+
+  render() {
+    const { displayDrawer, listNotifications } = this.props;
+    return (
+      <>
+        {!displayDrawer && (
+          <div className={css(styles.menuItem)}>
+            <p>Your notifications</p>
+          </div>
+        )}
+
+        {displayDrawer && (
+          <div className={css(styles.notifications)}>
+            <button
+              className={css(styles.closeBtn)}
+              aria-label="Close"
+              onClick={this.handleClose}
+            >
+              x
+            </button>
+            <p>Here is the list of notifications</p>
+            {listNotifications.length === 0 ? (
+              <NotificationItem
+                type="default"
+                value="No new notification for now"
+              />
+            ) : (
+              <ul className={css(styles.ul)}>
+                {listNotifications.map(notification => (
+                  <NotificationItem
+                    key={notification.id}
+                    type={notification.type}
+                    value={notification.value}
+                    html={notification.html}
+                    markAsRead={this.markAsRead}
+                    id={notification.id}
+                  />
+                ))}
+              </ul>
+            )}
+          </div>
+        )}
+      </>
+    );
+  }
+}
+
+const opacityFrames = {
+  '0%': { opacity: 0.5 },
+  '100%': { opacity: 1 }
+};
+
+const bounceFrames = {
+  '0%': { transform: 'translateY(0)' },
+  '50%': { transform: 'translateY(-5px)' },
+  '100%': { transform: 'translateY(5px)' }
+};
+
+const styles = StyleSheet.create({
+  notifications: {
+    position: 'absolute',
+    border: 'dashed #cf4550 2px',
+    padding: '10px',
+    width: '30%',
+    right: '1rem',
+    top: '4rem',
+    '@media (max-width: 900px)': {
+      inset: 0,
+      width: '100%',
+      height: '100%',
+      border: 'none',
+      padding: 0,
+      backgroundColor: 'white',
+    },
+  },
+  ul: {
+    '@media (max-width: 900px)': {
+      margin: 0,
+      padding: 0,
+      listStyle: 'none',
+      fontSize: '20px',
+    },
+  },
+  menuItem: {
+    position: 'fixed', // Float above other elements
+    right: '10px',
+    top: '10px',
+    backgroundColor: '#fff8f8', // Light red background
+    padding: '10px',
+    cursor: 'pointer', // Change cursor to pointer on hover
+    ':hover': {
+      animationName: [opacityFrames, bounceFrames],
+      animationDuration: '1s, 0.5s',
+      animationIterationCount: '3',
+      animationTimingFunction: 'ease-in-out',
+    },
+  },
+  closeBtn: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    background: 'none',
+    border: 'none',
+    fontSize: '20px',
+    cursor: 'pointer',
+    '@media (max-width: 900px)': {
+      top: 0,
+      right: '.5rem'
+    },
+  }
+});
+
+export default Notifications;
