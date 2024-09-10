0118152eb2fe169f7031e04f57827b14b39f7f9a
MarcP70 <marcpourias@yahoo.fr>
2024-09-10 12:17:43 +0200

    Add files for task 2

---
+++ react_state/task_2/dashboard/src/App/App.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Notifications from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList';
+import BodySection from '../BodySection/BodySection';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import { getLatestNotification } from '../utils/utils';
+import WithLogging from '../HOC/WithLogging'
+import AppContext, { defaultUser } from './AppContext';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayDrawer: false,
+      user: defaultUser,
+    };
+
+    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
+    this.handleHideDrawer = this.handleHideDrawer.bind(this);
+    this.logIn = this.logIn.bind(this);
+    this.logOut = this.logOut.bind(this);
+  };
+
+  handleDisplayDrawer () {
+    this.setState({ displayDrawer: true });
+  };
+
+  handleHideDrawer () {
+    this.setState({ displayDrawer: false });
+  };
+
+  logIn(email, password) {
+    // Update user state when login is successful
+    this.setState({
+      user: {
+        email: email,
+        password: password,
+        isLoggedIn: true,
+      },
+    });
+  }
+
+  logOut() {
+    // Reset user state when logging out
+    alert('Logging you out');
+    this.setState({
+      user: defaultUser,
+    });
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  };
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  };
+
+  handleKeyDown = event => {
+    if (event.ctrlKey && event.key === 'h') {
+      event.preventDefault();
+      this.logOut();
+    };
+  };
+
+  render() {
+    const { displayDrawer, user } = this.state;
+    const isIndex = true
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 }
+    ]
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 3, type: 'urgent', html: { __html: getLatestNotification() } }
+    ]
+
+    const LoginWithLogging = WithLogging(Login)
+    const LoginWithNotifications = WithLogging(Notifications)
+
+    const contextValue = { user, logOut: this.logOut };
+
+    return (
+      <AppContext.Provider value={contextValue}>
+        <>
+          <LoginWithNotifications
+            listNotifications={listNotifications}
+            displayDrawer={displayDrawer}
+            handleDisplayDrawer={this.handleDisplayDrawer}
+            handleHideDrawer={this.handleHideDrawer}
+          />
+          <div className={css(styles.app)}>
+            <Header />
+            {this.state.user.isLoggedIn ? (
+              <BodySectionWithMarginBottom title="Course list">
+                <CourseList listCourses={listCourses} />
+              </BodySectionWithMarginBottom>
+            ) : (
+              <BodySectionWithMarginBottom title="Log in to continue">
+                <LoginWithLogging logIn={this.logIn} />
+              </BodySectionWithMarginBottom>
+            )}
+            <BodySection title="News from the School">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, iure nisi, at suscipit ratione quos nulla vitae distinctio, quisquam soluta adipisci fuga impedit a doloremque. Aspernatur dolorum possimus quo numquam.
+              </p>
+            </BodySection>
+            <div className={css(styles.footer)}>
+              <Footer isIndex={isIndex} />
+            </div>
+          </div>
+        </>
+      </AppContext.Provider>
+    )
+  }
+}
+
+// Define styles using Aphrodite
+const styles = StyleSheet.create({
+  app: {
+  },
+  footer: {
+    borderTop: '4px solid #cf4550',
+    width: '100%',
+    bottom: '0',
+    left: '0',
+    textAlign: 'center',
+    fontSize: '20px',
+    fontStyle: 'italic',
+    fontFamily: 'Arial, sans-serif',
+  },
+});
+
+export default App;
